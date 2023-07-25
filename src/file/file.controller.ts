46d7307df93cbe3826485426998365f5fb508a16
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-07-25 20:10:45 +0530

    aws

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -15,7 +15,6 @@ import { Response } from "express"
 import * as fs from "fs"
 import { writeFile } from "fs/promises"
 import * as path from "path"
-import { S3 } from "aws-sdk"
 
 @Controller("files")
 export class FileController {
@@ -26,28 +25,20 @@ export class FileController {
 
   @Get(":filename")
   async getFile(@Param("filename") filename: string, @Res() res: Response) {
-    const s3 = new S3()
-    const bucketName = "nestjs-bhumfile-bucket"
-
-    const params = {
-      Bucket: bucketName,
-      Key: filename,
-    }
-
-    try {
-      const fileStream = s3.getObject(params).createReadStream()
-      res.setHeader("Content-Type", "application/pdf")
-      res.setHeader("Content-Disposition", `attachment; filename=${filename}`)
-      fileStream.pipe(res)
-    } catch (error) {
-      console.error("Error fetching file from AWS S3:", error)
+    const filePath = path.join(__dirname, "../..", "files", filename)
+    if (!fs.existsSync(filePath)) {
       return res.status(404).send("File not found")
     }
+
+    res.setHeader("Content-Type", "application/pdf")
+    res.setHeader("Content-Disposition", `attachment; filename=${filename}`)
+    fs.createReadStream(filePath).pipe(res)
   }
 
   @Put(":filename")
   @UseInterceptors(FileInterceptor("pdf"))
   async saveFile(@Body() requestBody: { pdfData: number[] }) {
+    console.log({ data: requestBody.pdfData })
     const uint8Array = new Uint8Array(requestBody.pdfData) // Convert array back to Uint8Array
     try {
       const filePath = `files/example.pdf`
@@ -65,6 +56,7 @@ export class FileController {
 
       return "PDF saved successfully"
     } catch (error) {
+      console.error("Error saving file:", error)
       throw new Error("Failed to save file")
     }
   }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common"
 
 @Injectable()
 export class FileService {
   getHello(): {} {
-      return { helloWorld: 'Hello World!' };
+    return { helloWorld: "Hello World!" }
   }
 }
