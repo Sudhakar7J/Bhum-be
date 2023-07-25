302e9b0a0a9e10969b40d49d293f47cc35692193
EC2 Default User <ec2-user@ip-172-31-38-238.ap-south-1.compute.internal>
2023-07-25 19:19:25 +0000

    Hosted via AWS EC2 and RDS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,27 @@ import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { FileModule } from "./file/file.module"
 import { TypeOrmModule } from "@nestjs/typeorm"
-import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Loads environment variables from .env or system environment
     TypeOrmModule.forRoot({
-      type: 'postgres', // Replace with your database type (e.g., mysql, postgres, etc.)
-      host: process.env.DB_HOST || 'localhost', // Replace with your RDS endpoint
-      port: parseInt(process.env.DB_PORT, 10) || 5432, // Replace with your database port
-      username: process.env.DB_USERNAME || 'username', // Replace with your database username
-      password: process.env.DB_PASSWORD || 'password', // Replace with your database password
-      database: process.env.DB_NAME || 'dbname', // Replace with your database name
+      type: "postgres",
+      host: "postgres.cctzg6zjlm2h.ap-south-1.rds.amazonaws.com",
+      port: 5432,
+      username: "postgres",
+      password: "adminadmin",
+      database: "postgres",
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true,
        ssl: {
         rejectUnauthorized: false, // Set to true if your RDS instance has a valid SSL certificate
       },
       autoLoadEntities: true,
-      synchronize: true, // Note: Do not use synchronize in production, as it can cause data loss
     }),
+    FileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
