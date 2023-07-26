60769fc43accee6320aaf14a1ce680202b980d86
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-07-26 06:40:06 +0530

    railway hosting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import * as express from "express"
-import { FilledPDF } from "./file/file.entity"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  await app.listen(3000)
+  app.use(express.json({ limit: "50mb" }))
+  await app.listen(4000)
 }
 bootstrap()
